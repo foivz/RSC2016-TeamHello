0f3a92df7de9a9af700606a8fc5b73c9230afaf1
Petar Trutanić <Petar Trutani>
2016-11-26 17:07:19 +0100

    minor  changes to event

---
+++ QuizApp/wwwroot/scripts/app/event/myEvents/myEvents.controller.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+angular.module('app').controller('myEventsController', MyEventsController);
+
+MyEventsController.$inject = ['$state','eventService'];
+
+function MyEventsController($state, eventService,loginService) {
+    var vm = this;
+
+    vm.a = "angularCheck";
+
+
+
+    vm.futureEvents = eventService.getFutureEventsByMod();
+
+
+    
+
+};
