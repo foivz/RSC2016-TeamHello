737ac3347dc785d8a3f34376848a9a6aade168cc
wr9 <asikim00@fesb.hr>
2016-11-26 13:50:42 +0100

    qwe

---
+++ QuizApp/wwwroot/scripts/app/event/createEvent/createEvent.controller.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular.module('app').controller('createEventController', CreateEventController);
+
+CreateEventController.$inject = ['$state', 'eventService'];
+
+function CreateEventController($state, eventService) {
+    var vm = this;
+
+    vm.event = {};
+
+    vm.createEvent = function () {
+        eventService.creatEvent(vm.event)
+        .then(function (result) {
+            var eventID = result.data;
+
+            return $state.go('getEvent', { id: eventID });
+        });
+    }
+};
---
+++ QuizApp/wwwroot/scripts/app/event/createEvent/createEvent.state.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+angular.module('app').config(CreateEventStateProvider);
+
+CreateEventStateProvider.$inject = ['$stateProvider'];
+
+function CreateEventStateProvider($stateProvider) {
+    $stateProvider
+    .state('createEvent', {
+        url: '/create-event',
+        templateUrl: '/./../../scripts/app/event/createEvent/createEvent.template.html',
+        controller: 'createEventController',
+        controllerAs: 'vm',
+        params: { userID: null}
+    });
+};
