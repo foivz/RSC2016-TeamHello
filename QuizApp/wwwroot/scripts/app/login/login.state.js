0034eb7ac17c5d68f420d719b808d77253ff8122
Petar Trutanić <Petar Trutani>
2016-11-27 02:19:07 +0100

    started work on main screen

---
+++ QuizApp/wwwroot/scripts/app/login/login.state.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+angular.module('app').config(loginStateProvider);
+
+loginStateProvider.$inject = ['$stateProvider'];
+
+function loginStateProvider($stateProvider) {
+    $stateProvider
+    .state('login', {
+        url: '/login',
+        templateUrl: '/./../../scripts/app/login/login.template.html'
+    });
+};
